4973d14a59c08a777c4c6933ab597ff0f202f849
Karen <karenl.harper88@gmail.com>
2018-07-05 21:52:06 -0700

    Moved axis to middle, various cosmetics

--- SmithChart.pro
+++ SmithChart.pro
@@ -1,5 +1,7 @@
 #Qt Project file--for use by qmake
 
+CONFIG += qwt
+
 QT += core gui
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
--- window.cpp
+++ window.cpp
@@ -1,14 +1,21 @@
 #include "window.h"
+#include <qwt/qwt_plot_scaleitem.h>
 #include <QLineEdit>
 #include <cmath> 
 
 Window::Window() : plot(), charImp(50)  { // assume characteristic impedance is 50 Ohms
-    
-
+   
     vLayout.addWidget(&charImpSet);
     vLayout.addWidget(&real);
     vLayout.addWidget(&imagin);   
-   
+
+    plot.enableAxis(QwtPlot::Axis::xBottom, false);
+    plot.setAxisScale(QwtPlot::Axis::xBottom, 0, 50);
+    plot.setAxisScale(QwtPlot::Axis::yLeft, -1, 1);
+
+    QwtPlotScaleItem *it1 = new QwtPlotScaleItem(QwtScaleDraw::BottomScale ,0.0);
+    it1->attach(&plot);
+
     plot.replot();
     plot.show();
     
